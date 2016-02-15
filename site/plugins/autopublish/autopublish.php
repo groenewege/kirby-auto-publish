ea4e1c939a76231bf43cf7d047d37020a215c201
Thomas Flad <mail@thomasflad.de>
2016-02-15 14:41:01 +0100

    Remove default config value
    With the default config value the auto publish for all pages will not work.

--- site/plugins/autopublish/autopublish.php
+++ site/plugins/autopublish/autopublish.php
@@ -3,7 +3,7 @@
 kirby()->hook('panel.page.create', 'run');
 
 function run( $page ) {
-	$templates = c::get('autopublish.templates', array('project', 'item'));
+	$templates = c::get('autopublish.templates');
 	if (!$templates || in_array($page->template(), $templates)) {
 		try {
 			$page->toggle('last');
