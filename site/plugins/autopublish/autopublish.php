edc37b16deb303d105f92d527e107f239c0aaaaf
Gunther Groenewege <gunther@groenewege.com>
2016-11-16 02:01:18 +0100

    toggle no longer seems to work, use sort directly

--- site/plugins/autopublish/autopublish.php
+++ site/plugins/autopublish/autopublish.php
@@ -4,7 +4,7 @@ kirby()->hook('panel.page.create', function($page) {
 	$templates = c::get('autopublish.templates');
 	if (!$templates || in_array($page->template(), $templates)) {
 		try {
-			$page->toggle('last');
+			$page->sort('last');
 		} catch(Exception $e) {
 			return response::error($e->getMessage());
 		}
